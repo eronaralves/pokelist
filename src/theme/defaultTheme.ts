f261492303ec6971ded42e452a2660918b958557
eronaralves <alveseronar@gmail.com>
2023-06-02 23:49:51 -0300

    feat: 🎸 create scaffold

---
+++ src/theme/defaultTheme.ts
@@ -0,0 +1,14 @@
+
+
+export const defaultTheme = {
+  colors: {
+    light: {
+      background: '#E5E5E5',
+
+      headingText: '#17171B',
+      text: '#747476',
+
+      input: '#F2F2F2'
+    }
+  }
+}
