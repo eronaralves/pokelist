d5adba8dd0a133e6db760688f5c0621eb64058d0
eronaralves <alveseronar@gmail.com>
2023-06-03 17:51:07 -0300

    refactor: 💡 ajudted type input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,10 +3,7 @@ import { TextInputProps } from 'react-native'
 // Styles
 import * as S from './styles';
 
-// Interfaces
-interface InputProps extends TextInputProps {}
-
-export function Input({ ...rest }: InputProps) {
+export function Input({ ...rest }: TextInputProps) {
   return (
     <S.Container >
       <S.Icon />
